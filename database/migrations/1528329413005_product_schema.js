4200b935fedba616f72d0efc3cf25866ece4fa85
Dendi Handian <dendihandian@gmail.com>
2018-06-11 15:26:13 +0700

    add elasticsearch index & mapping create in products table migration

--- database/migrations/1528329413005_product_schema.js
+++ database/migrations/1528329413005_product_schema.js
@@ -1,9 +1,23 @@
 'use strict'
 
+const Product = use('App/Models/Product')
 const Schema = use('Schema')
+const Env = use('Env')
+const elasticsearch = use('elasticsearch')
+
+// create elasticsearch client instance
+const esHost = Env.get('ELASTICSEARCH_HOST', '127.0.0.1')
+const esPort = Env.get('ELASTICSEARCH_PORT', '9200')
+const esLog = Env.get('ELASTICSEARCH_LOG', 'trace')
+
+const esClient = new elasticsearch.Client({
+  host: esHost + ':' + esPort,
+  log: esLog
+});
 
 class ProductSchema extends Schema {
-  up () {
+  async up () {
+    // create products table
     this.create('products', (table) => {
       table.increments()
       table.string('name')
@@ -14,11 +28,29 @@ class ProductSchema extends Schema {
       table.timestamps()
     })
 
-    // TODO: create elasticsearch products index and mapping
+    // create elasticsearch products index
+    await esClient.indices.create({
+      index: Product.INDEX,
+    });
+
+    // create elasticsearch products mapping
+    esClient.indices.putMapping({
+      index: Product.INDEX,
+      type: Product.TYPE,
+      body: {
+        properties: Product.PROPERTIES
+      }
+    });
+
   }
 
   down () {
-    // TODO: remove elasticsearch products index and mapping
+    // remove elasticsearch products index and mapping
+    esClient.indices.delete({
+      index: Product.INDEX,
+    });
+
+    // delete product table
     this.drop('products')
   }
 }
