1c69baa8d5ae8da60226029dd3a2196c882943af
Dendi Handian <dendihandian@gmail.com>
2018-06-11 15:40:42 +0700

    implement elasticsearch in product seeder

--- database/seeds/ProductSeeder.js
+++ database/seeds/ProductSeeder.js
@@ -10,15 +10,46 @@
 |
 */
 
+const Env = use('Env')
+const elasticsearch     = require('elasticsearch');
+const Product = use('App/Models/Product')
+
+// create elasticsearch client instance
+const esHost = Env.get('ELASTICSEARCH_HOST', '127.0.0.1')
+const esPort = Env.get('ELASTICSEARCH_PORT', '9200')
+const esLog = Env.get('ELASTICSEARCH_LOG', 'trace')
+const esClient = new elasticsearch.Client({
+  host: esHost + ':' + esPort,
+  log: esLog
+});
+
 const Factory = use('Factory')
 
 class ProductSeeder {
   async run () {
+    // create products to database
     const products = await Factory
       .model('App/Models/Product')
       .createMany(10)
 
-    // TODO: add products to elasticsearch products index
+    // add products to elasticsearch products index
+    if (products) {
+      products.forEach(function(product) {
+        esClient.index({
+          index: Product.INDEX,
+          type: Product.TYPE,
+          id: product.id,
+          body: {
+            id: product.id,
+            name: product.name,
+            slug: product.slug,
+            stock: product.stock,
+            price: product.price,
+            description: product.description,
+          }
+        });
+      });
+    }
   }
 }
 
