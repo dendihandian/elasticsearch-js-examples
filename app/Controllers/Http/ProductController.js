c02b03c6c1afb736877e2e9ace89a99d24dc9848
Dendi Handian <dendihandian@gmail.com>
2018-06-11 16:15:19 +0700

    implement elasticsearch in products CRUD endpoint

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -2,10 +2,21 @@
 
 const Product = use('App/Models/Product')
 const slugify = use('slugify')
+const Env = use('Env')
+const elasticsearch = use('elasticsearch')
+
+const esHost = Env.get('ELASTICSEARCH_HOST', '127.0.0.1')
+const esPort = Env.get('ELASTICSEARCH_PORT', '9200')
+const esLog = Env.get('ELASTICSEARCH_LOG', 'trace')
+const esClient = new elasticsearch.Client({
+  host: esHost + ':' + esPort,
+  log: esLog
+})
 
 class ProductController {
   async index ({ response }) {
     const products = await Product.all()
+
     response.status(200).json({
       message: 'Here are your products',
       data: products
@@ -23,6 +34,21 @@ class ProductController {
 
     const product = await Product.create({ name, slug, stock, price, description })
 
+    // elasticsearch create
+    esClient.index({
+      index: Product.INDEX,
+      type: Product.TYPE,
+      id: product.id,
+      body: {
+        id: product.id,
+        name: product.name,
+        slug: product.slug,
+        stock: product.stock,
+        price: product.price,
+        description: product.description,
+      }
+    })
+
     response.status(201).json({
       message: 'Successfuly created a new product',
       data: product
@@ -30,11 +56,15 @@ class ProductController {
   }
 
   async show ({ request, response, params: {id} }) {
-    const product = request.post().product
+    const product = await esClient.get({
+      index: Product.INDEX,
+      type: Product.TYPE,
+      id: id
+    })
 
     response.status(200).json({
       message: 'Here is your product',
-      data: product
+      data: product._source
     })
   }
 
@@ -53,6 +83,23 @@ class ProductController {
 
     await product.save()
 
+    // elasticsearch update
+    esClient.update({
+      index: Product.INDEX,
+      type: Product.TYPE,
+      id: id,
+      body: {
+        doc: {
+          id: product.id,
+          name: product.name,
+          slug: product.slug,
+          stock: product.stock,
+          price: product.price,
+          description: product.description,
+        }
+      }
+    })
+
     response.status(200).json({
       message: 'Successfuly updated the product',
       data: product
@@ -63,6 +110,13 @@ class ProductController {
     const product = request.post().product
 
     await product.delete()
+
+    // elasticsearch delete
+    esClient.delete({
+      index: Product.INDEX,
+      type: Product.TYPE,
+      id: id,
+    })
 
     response.status(200).json({
       message: 'Successfuly deleted the product',
