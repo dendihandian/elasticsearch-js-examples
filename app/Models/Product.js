cf9b52a577919d28ac7fbcde4a2f73b88f8e3e33
Dendi Handian <dendihandian@gmail.com>
2018-06-11 15:25:29 +0700

    add Product constants for elasticsearch

--- app/Models/Product.js
+++ app/Models/Product.js
@@ -5,4 +5,15 @@ const Model = use('Model')
 class Product extends Model {
 }
 
+Product.INDEX = 'products'
+Product.TYPE = 'products'
+Product.PROPERTIES = {
+  id: { type: 'integer' },
+  name: { type: 'text' },
+  slug: { type: 'text' },
+  stock: { type: 'integer' },
+  price: { type: 'integer' },
+  description: { type: 'text' }
+}
+
 module.exports = Product
